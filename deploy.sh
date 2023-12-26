29d59f9077b244b822de8b90b10c61b446e742b6
Lluís Bosch <holalluis@gmail.com>
2023-12-26 23:55:57 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,6 @@ git push
 # -P  mostra progrés
 # -vv incrementa verbositat
 # -r  actua recursivament
-rsync --rsync-path="sudo rsync" -h -P -vv -r . debian@51.91.250.220:/var/www/html/llista-youtube
+rsync --rsync-path="sudo rsync" -h -P -vv -r \
+  --exclude ".git" \
+  . debian@51.91.250.220:/var/www/html/llista-youtube
